3ccdbfc3b83e210c14d89656f94c46f7e1ad802c
ashoo981 <34433276+ashoo981@users.noreply.github.com>
2017-12-11 10:42:15 +0300

    Update geometry.sql

--- postgresql/geometry.sql
+++ postgresql/geometry.sql
@@ -1,2 +1,15 @@
-select * from geomahalle
+alter TABLE "TASDIGER" alter column "Poly" type geometry(GEOMETRY)
+ 
+alter TABLE "TASDIGER" alter column "Poly" type geometry(geometry,2321)
+ 
+select * from "WFSMAHALLE"
+ 
+SELECT geometrytype("Poly") as g FROM "WFSMAHALLE" group by g
+ 
+SELECT st_Srid("Poly") as g FROM "TASDIGER" group by g
+ 
+update  "TASDIGER" set "Poly" =st_setsrid("Poly",2321)
+ 
+SELECT * FROM geometry_columns
+select * from "TASDIGER"
 
